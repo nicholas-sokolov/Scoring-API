da1bbaef56b597e5e8fb7635dacfee8c6c222a7c
Nicholas Sokolov <sokolov.nicholas@gmail.com>
2019-02-12 22:32:33 +0700

    added test of invalid request

--- tests/test.py
+++ tests/test.py
@@ -1,21 +1,33 @@
-import unittest
+import pytest
 
 import api
 
-
-class TestSuite(unittest.TestCase):
-    def setUp(self):
-        self.context = {}
-        self.headers = {}
-        self.store = None
-
-    def get_response(self, request):
-        return api.method_handler({"body": request, "headers": self.headers}, self.context, self.store)
-
-    def test_empty_request(self):
-        _, code = self.get_response({})
-        self.assertEqual(api.INVALID_REQUEST, code)
+headers = {}
+context = {}
+store = {}
 
 
-if __name__ == "__main__":
-    unittest.main()
+def get_response(request):
+    return api.method_handler({"body": request, "headers": headers}, context, store)
+
+
+def test_empty_request():
+    _, code = get_response({})
+    assert api.INVALID_REQUEST == code
+
+
+@pytest.mark.parametrize('request', [
+    {'login': "m&m's", 'token': 'qwerty', 'method': "", 'arguments': {}},
+    {'token': 'qwerty', 'method': "online_score", 'arguments': {}},
+    {'login': "m&m's", 'method': "online_score", 'arguments': {}},
+    {'login': "m&m's", 'token': 'qwerty', 'method': "online_score"},
+    {'login': "m&m's", 'token': 'qwerty', 'arguments': {}},
+    {'login': "m&m's", 'token': 'qwerty', 'method': "online_score", 'arguments': "123"},
+    {'login': "m&m's", 'token': 'qwerty', 'method': "online_score", 'arguments': 123},
+    {'login': 123, 'token': 'qwerty', 'method': "online_score", 'arguments': {}},
+    {'login': "m&m's", 'token': 123, 'method': "online_score", 'arguments': {}},
+    {'login': "m&m's", 'token': 'qwerty', 'method': 123, 'arguments': {}},
+])
+def test_invalid_request(request):
+    response, code = get_response(request)
+    assert api.INVALID_REQUEST == code
